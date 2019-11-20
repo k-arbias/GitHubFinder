e7ff51d5c8a36972b5b8d535277a0cad98e9f752
Arbias <52889109+k-arbias@users.noreply.github.com>
2019-11-20 20:43:43 +0100

    Fixed one bug with repos

--- github.js
+++ github.js
@@ -10,8 +10,7 @@ class Github{
         const profileResponse = await fetch(`https://api.github.com/users/${user}
         ?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
-        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?
-        per_page=${this.repos_count}&sort=${this.repos_sort}
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}
         &client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
